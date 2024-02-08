4ce1c91505785bf9763fdc41a4a13a8b19496946
Ishita <ishitamun2000@gmail.com>
2024-02-09 02:36:37 +0530

    changes

--- server/src/app.js
+++ server/src/app.js
@@ -11,14 +11,12 @@ const app = express();
 
 
 app.use(express.json());
-var whitelist = ['http://localhost:3000/', 'https://xim-mcc.netlify.app/']
 app.use(cors({
     origin: '*'
 }));
 
 app.use('/',prelimsRouter);
 app.use('/',adminRouter);
-// app.use('/prelimsData', prelimsRouter);
 app.use('/', (req, res) => {
     res.send('Welcome to the server');
 })
